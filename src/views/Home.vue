7a51311b91a7b4ef35d25c5742b0524870dfe604
mrcadu <carlosvicc14@gmail.com>
2021-09-01 13:20:18 -0300

    Implementando modal de login e modal de cadastro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,12 +41,43 @@
         <v-col>
           <h1 style="color:black; font-family: cursive;">Não é uma dieta. Não é uma fase.<br/> É uma mudança permanente no estilo de vida</h1>
           <v-row class="mt-4" justify="center">
-            <v-btn class="mr-4 white--text" color="green" style="width: 180px">Cadastro</v-btn>
-            <v-btn color="white" class="green--text" style="width: 180px">Login</v-btn>
+            <v-btn class="mr-4 white--text" color="green" style="width: 180px" @click="dialogCadastro=true">Cadastro</v-btn>
+            <v-btn color="white" class="green--text" style="width: 180px" @click="dialogLogin=true">Login</v-btn>
           </v-row>
         </v-col>
       </v-card>
     </div>
+    <v-dialog max-width="600" v-model="dialogCadastro">
+      <v-card>
+        <v-card-title class="text-h5 green">
+          Realizar Cadastro
+        </v-card-title>
+        <v-form class="pa-8">
+          <v-text-field label="Email" v-model="email"/>
+          <v-text-field label="Usuário" v-model="usuario"/>
+          <v-text-field label="Senha" v-model="senha"/>
+          <v-row justify="center" class="mt-6">
+            <v-btn class="mr-4 white--text" color="red" style="width: 180px" @click="dialogCadastro=false">Cancelar</v-btn>
+            <v-btn class="white--text" color="green" style="width: 180px" @click="signup">Cadastrar</v-btn>
+          </v-row>
+        </v-form>
+      </v-card>
+    </v-dialog>
+    <v-dialog max-width="600" v-model="dialogLogin">
+      <v-card>
+        <v-card-title class="text-h5 green">
+          Realizar Cadastro
+        </v-card-title>
+        <v-form class="pa-8">
+          <v-text-field label="Usuário" v-model="usuario"/>
+          <v-text-field label="Senha" v-model="senha"/>
+          <v-row justify="center" class="mt-6">
+            <v-btn class="mr-4 white--text" color="red" style="width: 180px" @click="dialogLogin=false">Cancelar</v-btn>
+            <v-btn class="white--text" color="green" style="width: 180px" @click="signin">Logar</v-btn>
+          </v-row>
+        </v-form>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -55,10 +86,23 @@ export default {
   name: "HomePage",
   data:() => {
       return {
+        email:'',
+        usuario:'',
+        senha:'',
         tab:0,
+        dialogCadastro:false,
+        dialogLogin:false,
         headerItens:['Home', 'criar dieta', 'ver receitas', 'ver consumo', 'ver calorias'],
         menuItens: ['Fazer Login', 'Fazer Cadastro']
       }
+  },
+  methods: {
+    signup(){
+      this.dialogCadastro = false;
+    },
+    signin(){
+      this.dialogCadastro = false;
+    }
   }
 }
 
