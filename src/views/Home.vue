20243482fed363dd9264ff319aadaadc6668db30
mrcadu <carlosvicc14@gmail.com>
2021-10-12 19:43:34 -0300

    Generalizando header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,5 @@
 <template>
   <v-card>
-    <v-toolbar color="green" dark >
-        <v-tabs
-            v-model="tab"
-            align-with-title
-        >
-          <v-tabs-slider color="white"></v-tabs-slider>
-
-          <v-tab
-              v-for="item in headerItens"
-              :key="item"
-          >
-            {{ item }}
-          </v-tab>
-        </v-tabs>
-      <v-menu offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-avatar color="grey" v-bind="attrs" v-on="on">
-            <v-icon dark>
-              mdi-account-circle
-            </v-icon>
-          </v-avatar>
-        </template>
-        <v-list>
-          <v-list-item
-              v-for="(item, index) in menuItens"
-              :key="index"
-          >
-              <v-list-item-title style="cursor:pointer">{{ item }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-
-    </v-toolbar>
     <div class="mt-3">
       <v-row justify="center">
         <v-img src="@/assets/diet.jpg" max-height="1000" width="1080"/>
