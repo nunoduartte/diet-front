8b775fce326295d8c591bd9893701c5303f3caf5
ncorreia <ncorreia.ufrj@gmail.com>
2021-10-12 18:14:56 -0300

    Adiciona nova página para perfil do usuário.

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: SignUp
+  },
+  {
+    path: '/perfil',
+    name: 'Seus Dados',
+    component: SignIn
   }
 ]
 
