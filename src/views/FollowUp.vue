703cfce15a2c71bd092ef73d77e9e70b6645f11d
mrcadu <carlosvicc14@gmail.com>
2021-10-13 06:27:33 -0300

    Criando gráficos para o follow up

--- src/views/FollowUp.vue
+++ src/views/FollowUp.vue
@@ -13,6 +13,72 @@
         <v-text-field type="number" label="Quantidade" v-model="amount" suffix="g"/>
         <v-btn @click="addFood" color="success">Adicionar alimento</v-btn>
       </v-card>
+      <v-card
+          class="mx-auto text-center mt-10"
+          color="green"
+          dark
+      >
+        <h1>carboidratos</h1>
+        <v-card-text>
+          <v-sheet
+              color="rgba(0, 0, 0, .12)">
+            <v-sparkline
+                :labels="labels"
+                :value="values"
+                color="white"
+                line-width="4"
+                padding="16"
+            ></v-sparkline>
+          </v-sheet>
+        </v-card-text>
+        <v-card-text>
+          <span>{{`${carbohydrateConsumed}g de carboidratos de ${loggedUser.userData.diet.carbohydrate}g necessárias`}}</span>
+        </v-card-text>
+      </v-card>
+      <v-card
+          class="mx-auto text-center mt-10"
+          color="green"
+          dark
+      >
+        <h1>Proteínas</h1>
+        <v-card-text>
+          <v-sheet
+              color="rgba(0, 0, 0, .12)">
+            <v-sparkline
+                :labels="labels"
+                :value="values"
+                color="white"
+                line-width="4"
+                padding="16"
+            ></v-sparkline>
+          </v-sheet>
+        </v-card-text>
+        <v-card-text>
+          <span>{{`${proteinConsumed}g de proteína de ${loggedUser.userData.diet.protein}g necessárias`}}</span>
+        </v-card-text>
+      </v-card>
+      <v-card
+          class="mx-auto text-center mt-10"
+          color="green"
+          dark
+      >
+        <h1>Gordura</h1>
+        <v-card-text>
+          <v-sheet
+              color="rgba(0, 0, 0, .12)">
+            <v-sparkline
+                :labels="labels"
+                :value="values"
+                color="white"
+                line-width="4"
+                padding="16"
+            ></v-sparkline>
+          </v-sheet>
+        </v-card-text>
+        <v-card-text>
+          <span>{{`${fatConsumed}g de gordura de ${loggedUser.userData.diet.fat}g necessárias`}}</span>
+        </v-card-text>
+      </v-card>
     </v-col>
   </v-col>
 </template>
@@ -26,15 +92,26 @@ export default {
   name: "FollowUp",
   data(){
     return {
+      foodRegistries:[],
+      labels:[],
+      values:[],
       selectedFood: null,
       amount: 100,
       loading:false,
       search:null,
+      proteinConsumed:0,
+      fatConsumed:0,
+      carbohydrateConsumed:0,
       foods:[]
     }
   },
   computed: {
     ...mapState(["loggedUser"]),
+  },
+  created(){
+    FollowUpService.getFoodRegistry(this.loggedUser.id).then((res)=>{
+      this.foodRegistries = res.data;
+    })
   },
   methods:{
     addFood(){
@@ -47,11 +124,22 @@ export default {
         protein:(this.selectedFood.proteina/100)*this.amount,
         fat:(this.selectedFood.lipidios/100)*this.amount,
       }).then((res)=>{
-        console.log(res);
+        this.foodRegistries.push(res.data);
       })
     }
   },
   watch: {
+    foodRegistries(foods){
+      this.labels = [];
+      this.values = [];
+      foods.forEach((food)=>{
+        this.labels.push(new Date(food.registryDate).getHours() + "h");
+        this.carbohydrateConsumed += food.carbohydrate;
+        this.proteinConsumed += food.protein;
+        this.fatConsumed += food.fat;
+        this.values.push(this.carbohydrateConsumed);
+      })
+    },
     search (value) {
       if(value.length > 2) {
         this.loading=true;
@@ -68,5 +156,8 @@ export default {
 </script>
 
 <style scoped>
-
+.v-sheet--offset {
+  top: -24px;
+  position: relative;
+}
 </style>
