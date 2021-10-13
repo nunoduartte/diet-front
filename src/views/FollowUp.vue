2ce9f2f590e62ac57b2ea0335909bbe42caa985e
mrcadu <carlosvicc14@gmail.com>
2021-10-13 03:47:53 -0300

    Implementendo autocomplete corretamente

--- src/views/FollowUp.vue
+++ src/views/FollowUp.vue
@@ -2,18 +2,13 @@
   <v-col align="center">
     <v-col cols="8">
       <v-card class="pa-8">
-        <v-autocomplete label="Descrição" v-model="foodDescription" :items="foods" :loading="loading" :search-input.sync="search" cache-items>
-          <template v-slot:selection="data">
-            <v-sheet class="mx-1 my-1 pa-1" color="secondary" rounded>
-              <span class="mx-2 d-flex">{{ data.descricao}}</span>
-            </v-sheet>
-          </template>
-          <template v-slot:item="data">
-            <v-sheet class="mx-1 my-1 pa-1" color="secondary" rounded>
-              <span class="mx-2 d-flex">{{ data.descricao}}</span>
-            </v-sheet>
-          </template>
-        </v-autocomplete>
+        <v-autocomplete label="Descrição"
+                        v-model="foodDescription"
+                        :items="foods"
+                        :loading="loading"
+                        item-text="descricao"
+                        :search-input.sync="search"
+                        cache-items/>
         <v-text-field type="number" label="Quantidade" v-model="amount" suffix="g"/>
         <v-btn color="success">Adicionar alimento</v-btn>
       </v-card>
@@ -37,13 +32,15 @@ export default {
   },
   watch: {
     search (value) {
-      this.loading=true;
       if(value.length > 2) {
+        this.loading=true;
         FoodQueryService.query(value).then((res) => {
           this.foods = res.data;
+          this.loading=false;
         })
+      } else {
+        this.foods=[];
       }
-      this.loading=false;
     },
   },
 }
