f54e02c0719e1169648bdaeac8f7139c8554d57e
mrcadu <carlosvicc14@gmail.com>
2021-10-13 08:43:15 -0300

    Criando alteração e deleção de um registro de alimento

--- src/services/FollowUpService.js
+++ src/services/FollowUpService.js
@@ -4,7 +4,13 @@ export default {
   createFoodRegistry(foodRegistry){
     return Vue.axios.post("/foodRegistry",foodRegistry);
   },
+  updateFoodRegistry(foodRegistry){
+    return Vue.axios.put("/foodRegistry",foodRegistry);
+  },
   getFoodRegistry(userId){
     return Vue.axios.get(`/foodRegistry/${userId}`);
+  },
+  removeFoodRegistry(userId){
+    return Vue.axios.delete(`/foodRegistry/${userId}`)
   }
 }
