525f629f8a5721e50f9613b900c7471fd78ebc5e
mrcadu <carlosvicc14@gmail.com>
2021-10-13 04:24:23 -0300

    Criando a opção de adicionar um alimento ao histórico diário do usuário

--- src/views/FollowUp.vue
+++ src/views/FollowUp.vue
@@ -3,14 +3,15 @@
     <v-col cols="8">
       <v-card class="pa-8">
         <v-autocomplete label="Descrição"
-                        v-model="foodDescription"
+                        v-model="selectedFood"
                         :items="foods"
                         :loading="loading"
                         item-text="descricao"
+                        return-object
                         :search-input.sync="search"
                         cache-items/>
         <v-text-field type="number" label="Quantidade" v-model="amount" suffix="g"/>
-        <v-btn color="success">Adicionar alimento</v-btn>
+        <v-btn @click="addFood" color="success">Adicionar alimento</v-btn>
       </v-card>
     </v-col>
   </v-col>
@@ -18,16 +19,36 @@
 
 <script>
 import FoodQueryService from "@/services/FoodQueryService";
+import FollowUpService from "@/services/FollowUpService";
+import {mapState} from "vuex";
 
 export default {
   name: "FollowUp",
   data(){
     return {
-      foodDescription: null,
+      selectedFood: null,
       amount: 100,
       loading:false,
       search:null,
       foods:[]
+    }
+  },
+  computed: {
+    ...mapState(["loggedUser"]),
+  },
+  methods:{
+    addFood(){
+      FollowUpService.createFoodRegistry({
+        user:this.loggedUser,
+        description: this.selectedFood.descricao,
+        amount: this.amount,
+        code: this.selectedFood.codigo,
+        carbohydrate:(this.selectedFood.carboidratos/100)*this.amount,
+        protein:(this.selectedFood.proteina/100)*this.amount,
+        fat:(this.selectedFood.lipidios/100)*this.amount,
+      }).then((res)=>{
+        console.log(res);
+      })
     }
   },
   watch: {
