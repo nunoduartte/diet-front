88495517a8c31d2175e40cf895629aeb012b3111
ncorreia <ncorreia.ufrj@gmail.com>
2021-10-13 10:13:24 -0300

    Cria formulário para definição dos dados do usuário e chamadas para salvar e recuperar os dados do usuário.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,40 +1,5 @@
 <template>
   <div>
-    <v-card>
-      <v-toolbar color="green" dark >
-          <v-tabs
-              v-model="tab"
-              align-with-title
-          >
-            <v-tabs-slider color="white"></v-tabs-slider>
-
-            <v-tab
-                v-for="item in headerItens"
-                :key="item"
-            >
-              {{ item }}
-            </v-tab>
-          </v-tabs>
-        <v-menu offset-y>
-          <template v-slot:activator="{ on, attrs }">
-            <v-avatar color="grey" v-bind="attrs" v-on="on">
-              <v-icon dark>
-                mdi-account-circle
-              </v-icon>
-            </v-avatar>
-          </template>
-          <v-list>
-            <v-list-item
-                v-for="(item, index) in menuItens"
-                :key="index"
-            >
-                <v-list-item-title style="cursor:pointer">{{ item }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-
-      </v-toolbar>
-    </v-card>
     <v-col class="mt-4" align="center">
       <v-row class="mt-4" justify="center">
         <v-col cols="4">
